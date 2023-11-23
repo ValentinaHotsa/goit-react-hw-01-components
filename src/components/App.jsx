da0e365401c036ed499859a494a7aafc40548cbb
ValentinaHotsa <valentinahotsa@gmail.com>
2023-11-23 21:03:06 +0100

    task 1, task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,9 @@
+import user from '../data/user.json'
+import data from '../data/data.json'
+import friends from '../data/friends.json'
+import Profile from './profile.jsx'
+import Statistics from './Statistics.jsx'
+import { FriendList } from './FriendList.jsx'
 export const App = () => {
   return (
     <div
@@ -10,7 +16,46 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+     <Profile
+         username={user.username}
+         tag={user.tag}
+         location={user.location}
+         avatar={user.avatar}
+         stats={user.stats}
+      />
+     <Statistics title="Upload stats" stats={data} />
+     <FriendList friends={friends} />
     </div>
   );
 };
+
+
+
+
+// <div className="profile">
+//   <div className="description">
+//     <img
+//       src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+//       alt="User avatar"
+//       className="avatar"
+//     />
+//     <p className="name">Petra Marica</p>
+//     <p className="tag">@pmarica</p>
+//     <p className="location">Salvador, Brasil</p>
+//   </div>
+
+//   <ul className="stats">
+//     <li>
+//       <span className="label">Followers</span>
+//       <span className="quantity">1000</span>
+//     </li>
+//     <li>
+//       <span className="label">Views</span>
+//       <span className="quantity">2000</span>
+//     </li>
+//     <li>
+//       <span className="label">Likes</span>
+//       <span className="quantity">3000</span>
+//     </li>
+//   </ul>
+// </div>
